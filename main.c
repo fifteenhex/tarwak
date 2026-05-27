9a35b5fed4dabec898a8bfa52b55d8e33438f908
Daniel Palmer <daniel@thingy.jp>
2026-05-27 18:07:41 +0900

    Add libarchive, create an empty tarball

--- main.c
+++ main.c
@@ -2,13 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/* libarchive */
+#include <archive.h>
+#include <archive_entry.h>
+
 static void usage(const char *prog)
 {
 	fprintf(stderr, "usage: %s -i <input> -o <output> -b <basedir>\n", prog);
 }
 
+static void free_archive(struct archive **a)
+{
+	if (*a)
+		archive_write_free(*a);
+}
+
 int main(int argc, char **argv)
 {
+	struct archive __attribute__((cleanup(free_archive))) *tarball = NULL;
 	const char *basedir = NULL;
 	const char *output = NULL;
 	const char *input = NULL;
@@ -35,6 +46,17 @@ int main(int argc, char **argv)
 		usage(argv[0]);
 		return 1;
 	}
+
+	tarball = archive_write_new();
+	archive_write_set_format_pax_restricted(tarball);
+	archive_write_add_filter_none(tarball);
+
+	if (archive_write_open_filename(tarball, output) != ARCHIVE_OK) {
+		fprintf(stderr, "failed to open output: %s\n", archive_error_string(tarball));
+		return 1;
+	}
+
+	archive_write_close(tarball);
 
 	return 0;
 }
