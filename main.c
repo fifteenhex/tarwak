77a9c18166cfe322cd13587accb5f7759dfc5222
Daniel Palmer <daniel@thingy.jp>
2026-05-27 17:55:29 +0900

    Arg parsing

--- main.c
+++ main.c
@@ -1,4 +1,40 @@
-int main(int argv, char **argc)
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+static void usage(const char *prog)
+{
+	fprintf(stderr, "usage: %s -i <input> -o <output> -b <basedir>\n", prog);
+}
+
+int main(int argc, char **argv)
+{
+	const char *basedir = NULL;
+	const char *output = NULL;
+	const char *input = NULL;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "i:o:b:")) != -1) {
+		switch (opt) {
+		case 'i':
+			input = optarg;
+			break;
+		case 'o':
+			output = optarg;
+			break;
+		case 'b':
+			basedir = optarg;
+			break;
+		default:
+			usage(argv[0]);
+			break;
+		}
+	}
+
+	if (!input || !output || !basedir) {
+		usage(argv[0]);
+		return 1;
+	}
+
+	return 0;
 }
